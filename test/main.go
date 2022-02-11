1b83ce7787d5aa636c6cf07c812b1a77b6619934
dengpengju <dengpju@163.com>
2022-02-11 22:51:19 +0800

    更新

--- test/main.go
+++ test/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/dengpju/higo-wsock/wsock"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"log"
 )
 
@@ -13,12 +14,27 @@ func main() {
 	r.Handle("GET", "/conn", func(context *gin.Context) {
 		fmt.Println("hhh")
 		fmt.Println(context.Writer)
-		//wsock.WsRespString("ttt")
-		//wsock.WsConn(context).Conn().WriteMessage(websocket.TextMessage, []byte("ttt"))
-		//context.String(200, "fff")
+		loginEntity := NewLoginEntity()
+		err := context.ShouldBind(loginEntity)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Conn", loginEntity)
+		wsock.WsConn(context).Conn().WriteMessage(websocket.TextMessage, []byte("ttt"))
 	})
 	err := r.Run(":8080")
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+type LoginEntity struct {
+	UserName    string `json:"username" binding:"required"`
+	Password    string `json:"password" binding:"required"`
+	CaptchaCode string `json:"captcha_code" binding:"required"`
+	Time        string `json:"time" binding:"required"`
+}
+
+func NewLoginEntity() *LoginEntity {
+	return &LoginEntity{}
 }
